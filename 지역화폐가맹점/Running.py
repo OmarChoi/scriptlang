86ab06779b3775a5b88900c6559371f3b4592cbd
SeoGJ <sgj9946@gmail.com>
2022-05-22 16:26:54 +0900

    SecondWeek

--- 지역화폐가맹점/Running.py
+++ 지역화폐가맹점/Running.py
@@ -14,6 +14,11 @@ INPUT_CMPNM_NM = None
 leftListBox = None
 rightListBox = None
 
+photo0 = PhotoImage(file = "Search.PNG")
+photo1 = PhotoImage(file = "Homepage.PNG")
+photo2 = PhotoImage(file = "Map.png")
+photo3 = PhotoImage(file = "Mail.png")
+
 def event_for_listbox(event): # 리스트 선택 시 내용 출력
     global rightListBox
 
@@ -31,7 +36,7 @@ def event_for_listbox(event): # 리스트 선택 시 내용 출력
 
 def InitScreen():
     fontTitle = font.Font(g_Tk, size = 18, weight='bold', family='바탕체')
-    fontNormal = font.Font(g_Tk, size= 15,weight='bold')
+    fontNormal = font.Font(g_Tk, size= 15, weight='bold')
 
     frameTitle = Frame(g_Tk, padx=10, pady=10,bg='#1fffbf')
     frameTitle.pack(side="top", fill="x")
@@ -52,14 +57,14 @@ def InitScreen():
     MainText.pack(anchor="center", fill="both")
 
     global SIGUN_NM_Combo
-    SIGUN_NM_List = ['성남시', '가평군', '부천시', '시흥시'] 
-    SIGUN_NM_Combo = ttk.Combobox(frameClassification, font=fontNormal, width = 10, height = 1, values=SIGUN_NM_List) 
+    SIGUN_NM_List = ["가평군", "고양시", "과천시", "광명시", "광주시", "구리시", "군포시", "김포시", "남양주시", "동두천시", "부천시", "성남시", "수원시", "시흥시", "안산시", "안성시", "안양시", "양주시", "양평군", "여주시", "연천군", "오산시", "용인시", "의왕시", "의정부시", "이천시", "파주시", "평택시", "포천시", "하남시", "화성시"] 
+    SIGUN_NM_Combo = ttk.Combobox(frameClassification, font=fontNormal, width = 10, height = 10, values=SIGUN_NM_List, state='readonly')
     SIGUN_NM_Combo.pack(side='left', padx=10, expand=True, fill='both')
     SIGUN_NM_Combo.set("시/군")
 
     global INDUTYPE_NM_Combo
-    INDUTYPE_NM_List = ['소매업', '음식점업', '개인서비스업', '슈퍼/마트', '보건업'] 
-    INDUTYPE_NM_Combo = ttk.Combobox(frameClassification, font=fontNormal, width = 10, height = 1, values=INDUTYPE_NM_List) 
+    INDUTYPE_NM_List = ['소매업', '음식점업', '개인서비스업', '슈퍼/마트', '보건업', '교육서비스업', '기타'] 
+    INDUTYPE_NM_Combo = ttk.Combobox(frameClassification, font=fontNormal, width = 10, height = 1, values=INDUTYPE_NM_List, state='readonly') 
     INDUTYPE_NM_Combo.pack(side='right', padx=10, expand=True, fill='both')
     INDUTYPE_NM_Combo.set("업종분류")
 
@@ -67,8 +72,9 @@ def InitScreen():
     INPUT_CMPNM_NM = Entry(frameName, font=fontNormal, width=26,borderwidth=12, relief='ridge')
     INPUT_CMPNM_NM.pack(side="left", padx=10, expand=True, fill='both')
     
-    SearchButton = Button(frameName, font=fontNormal,text="검색", command=onSearch)
-    SearchButton.pack(side="right", padx=10, expand=True, fill='both')
+    global photo0
+    SearchButton = Button(frameName, image=photo0, command=onSearch)
+    SearchButton.pack(side="right", padx=10, expand=True, fill='y')
 
     global leftListBox
     leftListBox = Listbox(frameList, selectmode='extended', font = fontNormal, width=10, height=15, borderwidth= 12, relief= 'ridge')
@@ -79,16 +85,21 @@ def InitScreen():
     rightListBox = Listbox(frameList, font = fontNormal, width=10, height=15, borderwidth= 12, relief= 'ridge')
     rightListBox.pack(side='right', anchor='n', expand=True, fill="x")
 
+    global photo1, photo2, photo3
 
-    MapButton = Button(frameETC, font=fontNormal,text="지도")
-    MapButton.pack(side="right", padx=10, fill="y")
-
-    MailButton = Button(frameETC, font=fontNormal,text="메일")
-    MailButton.pack(side="right", padx=10, fill="y")
-
-    HomepageLink = Button(frameETC, font=fontNormal,text="경기지역화폐")
+    HomepageLink = Button(frameETC, image = photo1, command=OpenPage)
     HomepageLink.pack(side="left", padx=10, fill="y")
 
+    MapButton = Button(frameETC, image = photo2)
+    MapButton.pack(side="right", padx=10, fill="y")
+
+    MapButton = Button(frameETC, image = photo3)
+    MapButton.pack(side="right", padx=10, fill="y")
+    
+def OpenPage():
+    import webbrowser
+    url = "https://www.gmoney.or.kr/base/main/view"
+    webbrowser.open(url)
 
 def onSearch():
     global SIGUN_NM_Combo
@@ -104,9 +115,15 @@ def onSearch():
                 _text = str(i['Name']) + "/" + str(i['local']) + "/" + str(i['category']) + "/" + str(i['address_01']) + "/" + str(i['address_02'])
                 leftListBox.insert(num - 1, _text)
         else:
-            if str(i['category']) == INDUTYPE_NM_Combo.get() and str(i['local']) == SIGUN_NM_Combo.get():
-                _text = str(i['Name']) + "/" + str(i['local']) + "/" + str(i['category']) + "/" + str(i['address_01']) + "/" + str(i['address_02'])
-                leftListBox.insert(num - 1, _text)
+            if str(INDUTYPE_NM_Combo.get() != "기타"):
+                if str(i['category']) == INDUTYPE_NM_Combo.get() and str(i['local']) == SIGUN_NM_Combo.get():
+                    _text = str(i['Name']) + "/" + str(i['local']) + "/" + str(i['category']) + "/" + str(i['address_01']) + "/" + str(i['address_02'])
+                    leftListBox.insert(num - 1, _text)
+            else:
+                if str(i['local']) == SIGUN_NM_Combo.get():
+                    _text = str(i['Name']) + "/" + str(i['local']) + "/" + str(i['category']) + "/" + str(i['address_01']) + "/" + str(i['address_02'])
+                    leftListBox.insert(num - 1, _text)
+                    
 getData()
 InitScreen()
 g_Tk.mainloop()
