b32f02fc8fa6d42c7e7da33c66cfc77309e14bb5
SeoGJ <sgj9946@gmail.com>
2022-06-04 15:25:34 +0900

    C/C++ 연동

---
+++ 지역화폐가맹점/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup, Extension
+
+module_spam = Extension('spam',
+    sources=['spammodule.c'])
+
+setup(
+    name='Running', 
+    version='1.0',
+
+    py_modules=['Running', 'APIConnect'],
+
+    packages=['image'],
+    package_data = {'image': ['*.PNG']},
+
+    ext_modules=[module_spam]
+)
